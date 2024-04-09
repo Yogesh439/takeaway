3af11824a477a9aeaf853bb91619bfafa0fb8a06
yogesh439 <sharma.yogesh1015@gmail.com>
2024-04-09 17:05:16 +0545

    draggable marker icon not showing problem solved

--- client/src/components/map.js
+++ client/src/components/map.js
@@ -3,11 +3,20 @@ import 'leaflet/dist/leaflet.css';
 import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
 import L from 'leaflet';
 import 'leaflet-routing-machine';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
 const Map = () => {
   useEffect(() => {
     // Initialize Leaflet map
-    const map = L.map('map').setView([51.505, -0.09], 13);
+    const map = L.map('map').setView([27.679692, 85.330051], 13);
+    
+    // Specify the path to the default icon images
+    delete L.Icon.Default.prototype._getIconUrl;
+    L.Icon.Default.mergeOptions({
+      iconUrl: icon,
+      shadowUrl: iconShadow
+    });
 
     // Add tile layer
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -16,13 +25,13 @@ const Map = () => {
     }).addTo(map);
 
     // Add marker
-    L.marker([51.5, -0.09]).addTo(map);
+    // L.marker([27.679692, 85.330051]).addTo(map);
 
     // Add routing control
     L.Routing.control({
       waypoints: [
-        L.latLng(57.74, 11.94),
-        L.latLng(57.6792, 11.949)
+        L.latLng(27.679692, 85.330051),
+        L.latLng(27.683083, 85.323265)
       ]
     }).addTo(map);
 
@@ -35,7 +44,7 @@ const Map = () => {
   return (
     <div>
       <h1>Map</h1>
-      <div id="map" style={{ height: '500px', width: '100%', margin: 'auto', display: 'flex' }}></div>
+      <div id="map" style={{ height: '500px', width: '1000px', margin: 'auto', display: 'flex' }}></div>
     </div>
   );
 };
